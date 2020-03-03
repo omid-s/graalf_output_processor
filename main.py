b8a49b085eafe73c36f4a37c9271cc13933f60a1
Omid <omid.setayeshfar@msn.com>
2020-03-03 17:42:09 -0500

    reformats teh main file

--- main.py
+++ main.py
@@ -13,20 +13,20 @@ This main can be called using the following keywords :
 import argparse
 from src.__main__ import main
 
-
-parser = argparse.ArgumentParser(description="", add_help='How to use', prog='python main.py <options>')
+parser = argparse.ArgumentParser(description="Processes GrAALF's output into the path or connected components model",
+                                 add_help='How to use', prog='python main.py <options>')
 
 parser.add_argument("-i", "--input", dest="input_name", default=None,
-    help="Input file to be processed")
+                    help="Input file to be processed")
 
 parser.add_argument("-o", "--output-dir", dest="output_dir", default=None,
-    help="output directory to store files")
+                    help="output directory to store files")
 
 parser.add_argument("-p", "--path", dest="path_based", default=False, action="store_true",
-    help="to process the input signatures and process output with paths")
+                    help="to process the input signatures and process output with paths")
 
 parser.add_argument("-c", "--components", dest="component_based", default=False, action="store_true",
-    help="to process the input signatures and process output with connected components")
+                    help="to process the input signatures and process output with connected components")
 
 args = parser.parse_args()
 
